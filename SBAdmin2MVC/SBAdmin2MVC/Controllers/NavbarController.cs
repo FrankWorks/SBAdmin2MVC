14385b39d7cad927e8b4e10d5d548b697711579b
FrankWorks <frankfax@hotmail.com>
2017-08-27 01:23:13 -0700

    Initial Commit

---
+++ SBAdmin2MVC/SBAdmin2MVC/Controllers/NavbarController.cs
@@ -0,0 +1,20 @@
+﻿using SBAdmin2MVC.Domain;
+using SBAdmin2MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SBAdmin2MVC.Controllers
+{
+    public class NavbarController : Controller
+    {
+        // GET: Navbar
+        public ActionResult Index()
+        {
+            var data = new Data();
+            return PartialView("_Navbar", data.navbarItems().ToList());
+        }
+    }
+}
---
+++ SBAdmin2MVC/SBAdmin2MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SBAdmin2MVC.MvcApplication" Language="C#" %>
